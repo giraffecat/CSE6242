b110483a4f7eda29df6b8c8f6833f3c9dffd52f0
hui <1018997102@qq.com>
2022-04-21 18:09:53 -0400

    backend submit

---
+++ node.js
@@ -0,0 +1,178 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors=require("cors");
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'zh1998501',
+  database : 'CSE6242'
+});
+connection.connect();
+
+
+
+app.use(cors()); 
+
+app.use(bodyParser());
+
+app.get('/institution', function (req, res) {
+  var promise = new Promise(function(resolve, reject){
+    var query = "select institution from topicBySchool GROUP BY institution";
+    connection.query(query, function (err, result) {
+    if(err){
+      console.log('[INSERT ERROR] - ',err.message);
+      return;
+    }        
+    data = result
+    resolve(data)  
+    // res.end(JSON.stringify(data));
+    });
+  }).then(data => {
+    res.end(JSON.stringify(data));
+  })
+})
+
+app.post('/institutionDetail', function (req, res) {
+  var promise = new Promise(function(resolve, reject){
+    var query = `select * from topicBySchool where institution = "${req.body.selected}"`;
+    connection.query(query, function (err, result) {
+    if(err){
+      console.log('[INSERT ERROR] - ',err.message);
+      return;
+    }    
+    console.log("result",result)    
+    data = result
+    resolve(data)  
+    // res.end(JSON.stringify(data));
+    });
+  }).then(data => {
+    res.end(JSON.stringify(data));
+  })
+})
+
+app.post('/GetInstitutionPaper', function (req, res) {
+  var promise = new Promise(function(resolve, reject){
+    var query = "select institution from topicBySchool GROUP BY institution";
+    connection.query(query, function (err, result) {
+    if(err){
+      console.log('[INSERT ERROR] - ',err.message);
+      return;
+    }        
+    data = result
+    resolve(data)  
+    // res.end(JSON.stringify(data));
+    });
+  }).then(data => {
+    res.end(JSON.stringify(data));
+  })
+})
+
+app.get('/GetInstitutionCategory', function (req, res) {
+  var promise = new Promise(function(resolve, reject){
+    var query = "select DISTINCT topic from topicBySchool";
+    connection.query(query, function (err, result) {   
+    let data = result
+    resolve(data)  
+    // res.end(JSON.stringify(data));
+    });
+  }).then(data => {
+    let ans = [];
+    let temp = JSON.parse(JSON.stringify(data))
+    for(let i = 0; i < temp.length; i++) {
+      console.log("ad",temp)
+      ans.push(temp[i].topic)
+    }
+    console.log("ans",ans)
+    res.end(JSON.stringify(ans));
+  })
+})
+ 
+ 
+app.post('/paperList', async function (req, res) {
+   console.log(req.body);
+  let data = {}
+  var promise = new Promise(function(resolve, reject){
+    var query = 'select * from paper limit ' + (req.body.curPage - 1) * 6 + ", 6";
+    connection.query(query, function (err, result) {
+    if(err){
+      console.log('[INSERT ERROR] - ',err.message);
+      return;
+    }        
+    data['data'] = result
+    resolve(data)  
+    // res.end(JSON.stringify(data));
+    });
+  }).then(data => {
+    var query = "select count(*) as total from paper";
+    connection.query(query, function (err, result) {
+      data['total'] = result;
+      res.end(JSON.stringify(data));
+    });
+  })
+  // connection.end();
+})
+
+app.post('/search', function (req, res) {
+  console.log(req.body);
+  let searchWord = req.body.searchWord;
+   let data = {}
+   var promise = new Promise(function(resolve, reject){
+      var query = "select * from paper where title like '%" + searchWord + "%'" + " limit " + (req.body.curPage - 1) * 6 + ", 6";
+      connection.query(query, function (err, result) {
+      if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+      }   
+      data['data'] = result
+      resolve(data);     
+    });
+  }).then(data => {
+      var query = "select count(*) as total from paper where title like '%" + searchWord + "%'";
+      connection.query(query, function (err, result) {
+      data['length'] = result;    
+      res.end(JSON.stringify(data));
+      console.log("final", data)
+    });
+  })
+})
+
+app.post('/GetInsitutionPaper', function (req, res) {
+  let institutions = req.body.institutions;
+  let curPage = req.body.curPage; // 传接口参数
+
+  let promise_arr = [];
+  for(let i = 0; i < institutions.length; i++) {
+    var promise = new Promise(function(resolve, reject){
+      var query = "select * from paper where affiliation = '" + institutions[i] +"'";
+      connection.query(query, function (err, result) {
+      if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+      }   
+      data = result
+      resolve(data);     
+      });
+    })
+    promise_arr.push(promise);
+  }   
+  Promise.all(promise_arr).then(values => {
+    let data = {};
+    values = values.flat();
+    values = values.filter(a => a != null)
+    data['total'] = values.length;
+    values = values.slice(curPage * 6, curPage * 6 + 6)
+    data['data'] = values;
+    res.end(JSON.stringify(data));
+  })
+})
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("visit http://%s:%s", host, port)
+ 
+})
